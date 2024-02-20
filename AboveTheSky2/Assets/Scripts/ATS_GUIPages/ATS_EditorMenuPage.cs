e71d2784094fae7790387b4e851acda6565a652b
Tim <tim11251994@gmail.com>
2024-02-20 12:20:42 +0800

    Add EditorMenuPage

---
+++ AboveTheSky2/Assets/Scripts/ATS_GUIPages/ATS_EditorMenuPage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATS
+{
+    public class ATS_EditorMenuPage : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ AboveTheSky2/Assets/Scripts/Editor/ATS_AutoHeader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATS
+{
+    public class ATS_AutoHeader : MonoBehaviour
+    {
+        const string Header =
+@"
+// ATS_AutoHeader
+// to change the auto header please go to RCG_AutoHeader.cs";
+        const string HeaderFormat =
+    @"// {0} : {1}";
+        /// <summary>
+        /// https://prasetion.medium.com/automatically-adding-namespace-in-unity-c-script-dccbb7b4e728
+        /// Adding automatically namespace in created script
+        /// Edit -> Project Settings -> Editor -> C# Project Generation Section, then add your name of namespace in Root Namespace
+        /// </summary>
+        const string NameSpaceName = "RCG";
+        public static void OnWillCreateAsset(string iNewFileMeta)
+        {
+            //Debug.LogWarning($"OnWillCreateAsset iNewFileMeta:{iNewFileMeta}");
+            //RCG_GameManager.RefreshFileInspectorsStatic();
+
+            string aFilePath = iNewFileMeta.Replace(".meta", "");
+            if (aFilePath.EndsWith(".cs"))
+            {
+                Debug.LogWarning("Create New File:" + aFilePath);
+                string aStr = System.IO.File.ReadAllText(aFilePath);
+                System.Text.StringBuilder aSB = new System.Text.StringBuilder();
+                aSB.AppendLine(Header);
+
+                aSB.AppendLine(string.Format(HeaderFormat, "Create time", System.DateTime.Now.ToString("MM/dd yyyy HH:mm")));// HH:mm
+                //aSB.AppendLine(string.Format(HeaderFormat, "Author", System.Security.Principal.WindowsIdentity.GetCurrent().Name));
+                aSB.Append(aStr);
+                System.IO.File.WriteAllText(aFilePath, aSB.ToString());
+            }
+        }
+    }
+}

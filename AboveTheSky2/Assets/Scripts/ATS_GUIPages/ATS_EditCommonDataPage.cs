191c4b028c4ccdb24418a2aa34692f2c9ea98c6c
Tim <tim11251994@gmail.com>
2024-02-21 16:13:04 +0800

    Update ModuleService

--- AboveTheSky2/Assets/Scripts/ATS_GUIPages/ATS_EditCommonDataPage.cs
+++ AboveTheSky2/Assets/Scripts/ATS_GUIPages/ATS_EditCommonDataPage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UCL.Core;
 using UCL.Core.EditorLib.Page;
 using UCL.Core.UI;
 using UnityEngine;
@@ -10,7 +11,7 @@ namespace ATS.Page
     public class ATS_EditCommonDataPage : UCL_CommonEditorPage
     {
         static public ATS_EditCommonDataPage Create() => UCL_EditorPage.Create<ATS_EditCommonDataPage>();
-
+        UCL_ObjectDictionary m_DataDic = new UCL_ObjectDictionary();
         public ATS_EditCommonDataPage()
         {
 
@@ -22,6 +23,11 @@ namespace ATS.Page
 
         protected override void ContentOnGUI()
         {
+            if (!UCL_ModuleService.Initialized)
+            {
+                return;
+            }
+            UCL_ModuleService.Ins.OnGUI(m_DataDic.GetSubDic("ModuleService"));
             //GUILayout.Label("Test", UCL_GUIStyle.LabelStyle);
             var aLabelStyle = UCL_GUIStyle.GetLabelStyle(Color.white, 18);
             var aButtonStyle = UCL_GUIStyle.GetButtonStyle(Color.white, 18);

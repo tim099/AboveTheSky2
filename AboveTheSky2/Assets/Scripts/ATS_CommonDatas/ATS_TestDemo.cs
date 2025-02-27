a4d590bd4533a4b91f97ea44cf1da772bac76025
Tim <tim11251994@gmail.com>
2025-02-27 23:53:17 +0800

    [refactor] CommonEditPage update localize & Update Demo

---
+++ AboveTheSky2/Assets/Scripts/ATS_CommonDatas/ATS_TestDemo.cs
@@ -0,0 +1,48 @@
+﻿
+// ATS_AutoHeader
+// to change the auto header please go to ATS_AutoHeader.cs
+using System.Collections.Generic;
+using UCL.Core;
+using UCL.Core.UI;
+using UnityEngine;
+
+namespace ATS
+{
+    public class ATS_TestDemo : UCL_Asset<ATS_TestDemo>
+    {
+        /// <summary>
+        /// 測試用
+        /// </summary>
+        public int m_TestInt = 3;
+        /// <summary>
+        /// 測試用
+        /// </summary>
+        public List<string> m_TestList;
+        /// <summary>
+        /// 測試用
+        /// </summary>
+        public Dictionary<string, int> m_TestDictionary;
+
+        public ATS_RegionEntry m_RegionEntry = new();
+        public ATS_TestDemoEntry m_TestDemoEntry = new();
+        public override void Preview(UCL_ObjectDictionary iDataDic, bool iIsShowEditButton = false)
+        {
+            GUILayout.BeginVertical();
+            if (iIsShowEditButton)
+            {
+                ShowEditButtonOnGUI();
+            }
+            GUILayout.Label($"m_TestInt:{m_TestInt}", UCL_GUIStyle.LabelStyle);
+
+            GUILayout.EndVertical();
+        }
+    }
+
+    public class ATS_TestDemoEntry : UCL_AssetEntryDefault<ATS_TestDemo>
+    {
+        public const string DefaultID = "Default2";
+
+        public ATS_TestDemoEntry() { m_ID = DefaultID; }
+        public ATS_TestDemoEntry(string iID) { m_ID = iID; }
+    }
+}
